a6bb50c42c646a5e1888d6a7443352909fd1d607
mardeevamarya <mardeeva.mar@yandex.ru>
2020-07-07 19:14:46 +0300

    Revert "changes"
    This reverts commit 36edc0e99eebc40cbc2f39821ce49583f2d1f6b0.

--- src/games/progression.js
+++ src/games/progression.js
@@ -6,22 +6,22 @@ const condition = 'What number is missing in the progression?';
 const progressionLength = 10;
 
 const makeProgression = (step, first) => {
-  const progression = [first];
+  const progr = [first];
   for (let i = 1; i < progressionLength; i += 1) {
-    const current = progression[i - 1] + step;
-    progression.push(current);
+    const temp = progr[i - 1] + step;
+    progr.push(temp);
   }
-  return progression;
+  return progr;
 };
 
 const genRoundData = () => {
   const step = getRandomInt(1, 10);
-  const first = getRandomInt(1, 100);
-  const progression = makeProgression(step, first);
+  const nach = getRandomInt(1, 100);
+  const progr = makeProgression(step, nach);
   const hiddenIndex = getRandomInt(0, progressionLength - 1);
-  const correctAnswer = String(progression[hiddenIndex]);
-  progression[hiddenIndex] = '..';
-  const question = progression.join();
+  const correctAnswer = String(progr[hiddenIndex]);
+  progr[hiddenIndex] = '..';
+  const question = progr.join();
   return [question, correctAnswer];
 };
 
